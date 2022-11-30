1cca8afba5106a311a39f51a3a851f4cb78e60cb
jacobaxo <jacobm783b@gmail.com>
2022-11-30 12:46:10 -0500

    adding dictonary sample

---
+++ dictonary sample1.py
@@ -0,0 +1,10 @@
+sample_1 = {
+    "rocky" : "moose",
+    "bullwinkle" : "squirrel", 
+    "mickey" : "mouse",
+    "baloo" : "bear"
+}
+
+print (sample_1)
+print(sample_1["rocky"])
+print(sample_1["baloo"])

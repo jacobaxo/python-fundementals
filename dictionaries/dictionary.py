0837e7d1c9550846b0c4862e4656f67b1c899d66
jacobaxo <jacobm783b@gmail.com>
2023-01-11 11:46:00 -0500

    done

---
+++ dictionaries/dictionary.py
@@ -0,0 +1,8 @@
+sample_1 = {
+    "rocky" : "Moose",
+    "bullswinkkle" : "squirrel",
+    "Mickey" : "mouse", 
+    "baloo" : "bear"
+}
+
+print(sample_1)
---
+++ sring and string methods/stringex.py
@@ -0,0 +1,5 @@
+a = """look at this cool little 
+thing i can
+do
+look look look."""
+print(a)
